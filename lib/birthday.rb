2a3c8d64fc33f4818ec07533747201da37bca6be
Dina Kendle <dina.kendle@gmail.com>
2020-04-04 03:26:25 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -16,5 +16,6 @@ def age_appropriate_birthday
     if age <10 
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     else
-      puts 
+      puts "You are too old for this."
+    end
 end
